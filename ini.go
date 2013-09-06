2692178dff231a07d02ab1e08dca6f50a36daadd
Vaughan Newton <vaughann@amazon.com>
2013-09-06 09:37:08 +0200

    Documented ErrSyntax.Source

--- ini.go
+++ ini.go
@@ -18,7 +18,7 @@ var (
 // ErrSyntax is returned when there is a syntax error in an INI file.
 type ErrSyntax struct {
 	Line   int
-	Source string
+	Source string // The contents of the erroneous line, without leading and trailing whitespace
 }
 
 func (e ErrSyntax) Error() string {
